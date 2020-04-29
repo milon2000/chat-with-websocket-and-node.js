3f3d7cfd86c5ede88e1175dbd6c98c2cd1860ad1
milon200 <milakozlowska2000@gmail.com>
2020-04-29 16:08:01 +0200

    styling + events (client-side)

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const WebSocket = require('ws');
-
+const WebSocket = require("ws");
 const ws = new WebSocket.Server({ port: 8080 });
+let users = [];
 
-ws.on('connection', (webSocket) => {
-   webSocket.on('message', (message) => {
-    console.log('received: ', message);
-   }); 
-   webSocket.send('something');
+ws.on("connection", (webSocket) => {
+  webSocket.on("message", (message) => {
+    message = JSON.parse(message);
+
+    if (message.type == "name") {
+      webSocket.personName = message.data;
+      users.push(webSocket.personName);
+      return;
+    }
+    console.log(users);
+    console.log("Received:", message);
+    ws.clients.forEach((client) => {
+      if (client != webSocket) {
+        client.send(
+          JSON.stringify({
+            name: webSocket.personName,
+            data: message.data,
+            users: webSocket.users,
+          })
+        );
+      }
+    });
+  });
+
+  webSocket.users = users;
+  console.log(users);
+  webSocket.on("close", () => {
+    users = users.filter((user) => {
+      return user !== webSocket.personName;
+    });
+    console.log(users);
+  });
 });
--- main.js
+++ main.js
@@ -1,28 +1,69 @@
-const ws = new WebSocket('ws://localhost:8080');
-const form = document.querySelector('form');
-/*const chat = document.getElementById('czat'); */
+const ws = new WebSocket("ws://localhost:8080");
+let name = prompt("Hello, what's your name?");
+const chat = document.getElementById("chat");
+const participants = document.getElementById("participants");
+const form = document.querySelector("form");
 
-
-ws.onopen = e => {
-    console.log('connected');
+ws.onopen = (e) => {
+  console.log("connected");
+  ws.send(
+    JSON.stringify({
+      type: "name",
+      data: name,
+    })
+  );
 };
 
-ws.onerror = (error) => {
-    console.log('error: ', error);
+ws.onclose = () => {
+  console.error("disconnected");
 };
 
+// to jest to, co otrzymuje
 ws.onmessage = (e) => {
-    console.log(e);
+  console.log("received", e.data);
+  let json = JSON.parse(e.data);
+  const li = document.createElement("li");
+  li.classList.add("userName");
+  let time = new Date();
+  li.innerHTML +=
+    json.name +
+    " " +
+    "<span>" +
+    time.getHours() +
+    ":" +
+    time.getMinutes() +
+    "</span>";
+  chat.append(li);
+  const li2 = document.createElement("li");
+  li2.classList.add("userMsg");
+  li2.innerHTML += json.data;
+  chat.append(li2);
+  console.log(json.users);
+  const li3 = document.createElement("li");
+  li3.classList.add("userName");
+  li3.innerHTML += json.users;
+  participants.append(li3);
 };
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-     var message = document.getElementById('message').value;
-    ws.send(message);
-    console.log(message);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let message = document.querySelector("#message").value;
+  //ws.send(message);
+  ws.send(
+    JSON.stringify({
+      type: "message",
+      data: message,
+    })
+  );
+  const li = document.createElement("li");
+  li.classList.add("userName");
+  let time = new Date();
+  li.innerHTML +=
+    "You " + "<span>" + time.getHours() + ":" + time.getMinutes() + "</span>";
+  chat.append(li);
+  const li2 = document.createElement("li");
+  li2.classList.add("userMsg");
+  li2.innerHTML += message;
+  chat.append(li2);
+  document.querySelector("#message").value = "";
 });
-
-
-
-
-
